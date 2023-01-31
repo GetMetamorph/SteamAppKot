c9b79a12bb4b356898f941a3fe925b425fb4617b
Driss Kanouni <32987211+GetMetamorph@users.noreply.github.com>
2023-01-31 12:27:33 +0100

    Add appCompatActivity

--- app/src/main/java/com/example/steamappkot/RegisterActivity.kt
+++ app/src/main/java/com/example/steamappkot/RegisterActivity.kt
@@ -1,4 +1,11 @@
 package com.example.steamappkot
 
-class RegisterActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
 }
