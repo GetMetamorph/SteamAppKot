906373884ca65de04d057081f92ba72ed00faf0d
Driss Kanouni <32987211+GetMetamorph@users.noreply.github.com>
2023-01-31 12:17:30 +0100

    Change font names + add activity_forgot_password and fix various bugs

--- app/src/main/java/com/example/steamappkot/MainActivity.kt
+++ app/src/main/java/com/example/steamappkot/MainActivity.kt
@@ -2,13 +2,11 @@ package com.example.steamappkot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 //        if (BuildConfig.DEBUG) {
 //            Firebase.database.useEmulator("10.0.2.2", 9000)
 //            Firebase.auth.useEmulator("10.0.2.2", 9099)
