2f47ba2f85d17cb843b1cddcc0bae259e3b24cd8
Driss Kanouni <32987211+GetMetamorph@users.noreply.github.com>
2023-02-01 17:29:50 +0100

    Add depedencies for coroutines + code for api

--- app/src/main/java/com/example/steamappkot/MainActivity.kt
+++ app/src/main/java/com/example/steamappkot/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.steamappkot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch(Dispatchers.Main) {
+
+            try {
+                val todos = withContext(Dispatchers.IO)
+                { val response = CallAPI.todos.await()
+                Log.i("tag", response.toString())}
+
+            } catch (e: Exception) {  }
+        }
 //        if (BuildConfig.DEBUG) {
 //            Firebase.database.useEmulator("10.0.2.2", 9000)
 //            Firebase.auth.useEmulator("10.0.2.2", 9099)
