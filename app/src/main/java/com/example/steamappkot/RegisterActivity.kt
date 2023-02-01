ee455576f60fdf35e36a10a3ed553b5c55769aa1
Driss Kanouni <32987211+GetMetamorph@users.noreply.github.com>
2023-02-01 12:26:45 +0100

    Add user to firestore db

--- app/src/main/java/com/example/steamappkot/RegisterActivity.kt
+++ app/src/main/java/com/example/steamappkot/RegisterActivity.kt
@@ -1,15 +1,20 @@
 package com.example.steamappkot
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.steamappkot.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
     private lateinit var auth: FirebaseAuth
+    private val db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,11 +39,30 @@ class RegisterActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
+            // Create a new user with a first and last name
+            val user = hashMapOf(
+                "username" to "$username",
+                "email" to "$email",
+                "password" to "$password",
+                "LikedGames" to MutableList<String>(0, {" "}),
+                "Wishlist" to  MutableList<String>(0, {" "}),
+            )
+
+
             auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         // Sign in success, update UI with the signed-in user's information
-                        val user = auth.currentUser
+                        //val user = auth.currentUser
+                        // Add a new document with a generated ID
+                        db.collection("users")
+                            .add(user)
+                            .addOnSuccessListener { documentReference ->
+                                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                            }
+                            .addOnFailureListener { e ->
+                                Log.w(TAG, "Error adding document", e)
+                            }
                         val intent = Intent(this, LoginActivity::class.java)
                         startActivity(intent)
                         finish()
