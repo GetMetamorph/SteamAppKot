edadbaca8ac890a3473074451cb58b292e1e4c98
Driss Kanouni <32987211+GetMetamorph@users.noreply.github.com>
2023-02-01 10:17:25 +0100

    Added forgot password functionnality

--- app/src/main/java/com/example/steamappkot/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/steamappkot/ForgotPasswordActivity.kt
@@ -2,10 +2,36 @@ package com.example.steamappkot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.steamappkot.databinding.ActivityForgotPasswordBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class ForgotPasswordActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityForgotPasswordBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
+        binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.forgotPasswordButton.setOnClickListener {
+            val email = binding.emailInput.text.toString()
+
+            if (email.isEmpty()) {
+                binding.alertMessage.text = "Veuillez remplir tous les champs"
+                return@setOnClickListener
+            }
+
+            auth.sendPasswordResetEmail(email)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        binding.alertMessage.text = "Un email de réinitialisation a été envoyé"
+                    } else {
+                        binding.alertMessage.text = "L'email n'existe pas"
+                    }
+                }
+        }
     }
 }
