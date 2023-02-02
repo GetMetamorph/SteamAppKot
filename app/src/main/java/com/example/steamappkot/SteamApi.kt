5bc6dbd55d27b464d6c132511680b8850290a5b5
Driss Kanouni <32987211+GetMetamorph@users.noreply.github.com>
2023-02-02 09:04:13 +0100

    Fix api call

--- app/src/main/java/com/example/steamappkot/SteamApi.kt
+++ app/src/main/java/com/example/steamappkot/SteamApi.kt
@@ -4,10 +4,25 @@ import android.util.Log
 import com.google.gson.annotations.SerializedName
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import kotlinx.coroutines.Deferred
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+
+data class SteamResponse(
+    @SerializedName("response")
+    val response: Response
+)
+
+data class Response(
+    @SerializedName("rollup_date")
+    val rollup_date: Int,
+    @SerializedName("ranks")
+    val ranks: List<Rank>
+)
+
 
 data class Rank (
     @SerializedName("rank")
@@ -19,26 +34,25 @@ data class Rank (
     @SerializedName("peak_in_game")
     val peak_in_game: Int
 )
-
-data class Response(
-    @SerializedName("rollup_date")
-    val rollup_date: Int,
-    @SerializedName("ranks")
-    val ranks: List<Rank>
-)
-
 val apiKey: String = System.getenv("STEAM_API_KEY")
 
 interface SteamAPI {
 
     //In get add the follow of the url ex: https://https://api.steampowered.com/ become https://https://api.steampowered.com/GetMostPlayedGames/
     @GET("ISteamChartsService/GetMostPlayedGames/v1/?")
-    fun getMostPlayedGames() : Deferred<Response>
+    fun getMostPlayedGames() : Deferred<SteamResponse>
 
     //https://api.steampowered.com/ISteamChartsService/GetMostPlayedGames/v1/?
 }
 
 object CallAPI {
+    private val okHttp = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level =
+                if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+        })
+        .build()
+
     private val api = Retrofit.Builder()
         .baseUrl("https://api.steampowered.com/")
         .addConverterFactory(
@@ -46,11 +60,11 @@ object CallAPI {
         .addCallAdapterFactory(
             CoroutineCallAdapterFactory()
         )
+        .client(okHttp)
         .build()
         .create(SteamAPI::class.java)
 
-    suspend fun getMostPlayedGames() : Response {
-        Log.i("info", "dd")
+    suspend fun getMostPlayedGames() : SteamResponse {
         return api.getMostPlayedGames().await()
     }
 }
