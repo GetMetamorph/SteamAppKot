cb47869a308d29e0cf40c9865686a22e88b0bbe8
Brandon-Blackstar <mor.bra34@gmail.com>
2023-02-03 02:03:20 +0100

    like empty screen and wishlist empty

---
+++ app/src/main/java/com/example/steamappkot/LikeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.steamappkot
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class LikeActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_like)
+
+    }
+}
---
+++ app/src/main/java/com/example/steamappkot/WishlistActivity.kt
@@ -0,0 +1,13 @@
+package com.example.steamappkot
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WishlistActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wishlist)
+
+    }
+}
